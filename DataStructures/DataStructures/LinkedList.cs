0ef66203da0707471366d2a94f66d111678f8f37
Joseph Kerkhof <musicaljoeker@icloud.com>
2018-12-09 22:27:07 -0600

    Adding Linked Lists

---
+++ DataStructures/DataStructures/LinkedList.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+public class LinkedList
+{
+    private class Node
+    {
+        public int value;
+        public Node next;
+        public Node(int value)
+        {
+            this.value = value;
+            next = null;
+        }
+    }
+
+    Node head;
+	public LinkedList(int value)
+	{
+        head = new Node(value);
+	}
+
+    public void Insert(int value)
+    {
+        Node insertMe = new Node(value);
+        insertMe.next = head;
+        head = insertMe;
+    }
+
+    public void DeleteHead()
+    {
+        if(head.next != null)
+        {
+            head = head.next;
+        }
+    }
+
+    public void DeleteTail()
+    {
+        Node current = head;
+        while(current.next.next != null)
+        {
+            current = current.next;
+        }
+        current.next = null; // Iterate thru the linked list
+    }
+
+    public void Remove(int value)
+    {
+        if(head.value == value)
+        {
+            DeleteHead();
+            return;
+        }
+
+        Node current = head;
+        while(current.next != null)
+        {
+            if(current.next.value == value)
+            {
+                current.next = current.next.next;
+                return;
+            }
+            current = current.next; // Iterate thru the linked list
+        }
+        Console.WriteLine($"{value} was not found in the linked list.");
+    }
+
+    public bool Contains(int value)
+    {
+        Node current = head;
+        while(current.next != null)
+        {
+            if(current.value == value)
+            {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    public void ToString()
+    {
+        Node current = head;
+        while(current != null)
+        {
+            Console.Write($"{current.value} -> ");
+            current = current.next;
+        }
+        Console.Write("null\n");
+    }
+}
---
+++ DataStructures/DataStructures/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+
+            Console.WriteLine("Welcome to my data structures review project.");
+
+            p.RunTheLinkedList();
+
+            Console.WriteLine("Press any key to quit...");
+            Console.ReadKey(true);
+        }
+
+        private void RunTheLinkedList()
+        {
+            LinkedList ll = new LinkedList(15);
+            // Inserting a few variables here
+            ll.Insert(16);
+            ll.Insert(17);
+            ll.Insert(18);
+            ll.Insert(19);
+            ll.Insert(20);
+            ll.ToString();
+
+            // Deleting the head
+            ll.DeleteHead();
+            ll.ToString();
+
+            // Deleting the tail
+            ll.DeleteTail();
+            ll.ToString();
+
+            // Deleting a specific element
+            ll.Remove(21); // Should say it wasn't found
+            ll.Remove(18); // It should find this one and delete it
+            ll.ToString();
+
+            Console.WriteLine("Does the linked list contain 17? " + ll.Contains(17)); // Should return true
+            Console.WriteLine("Does the linked list contain 3? " + ll.Contains(3)); // Should return false
+        }
+    }
+}
